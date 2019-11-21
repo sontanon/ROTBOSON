01823c56346293d2bf43e41ac79817ff9d86eb95
sontanon <sontanon@gmail.com>
2019-11-20 18:30:04 -0600

    Update parameter integer types to MKL_INT.

--- src/param.h
+++ src/param.h
@@ -7,51 +7,51 @@ config_t cfg;
 /* GRID */
 double dr      	= 0.0625;
 double dz      	= 0.0625;
-int NrInterior 	= 128;
-int NzInterior 	= 128;
-int NrTotal	= 130;
-int NzTotal	= 130;
-int dim        	= 16900;
-int ghost      	= 1;
-int order 	= 2;
+MKL_INT NrInterior 	= 128;
+MKL_INT NzInterior 	= 128
+MKL_INT NrTotal	= 130;
+MKL_INT NzTotal	= 130;
+MKL_INT dim       	= 16900;
+MKL_INT ghost     	= 1;
+MKL_INT order 	= 2;
 
 /* SCALAR FIELD PARAMETERS */
-int l		= 1;	
+MKL_INT l		= 1;	
 double m 	= 1.0;
 double psi0 	= 0.2;
 double sigmaR	= 4.0;
 double sigmaZ	= 4.0;
 double rExt	= 16.0;
 double w0 	= 0.7;
-int fixedPhiR 	= 1;
-int fixedPhiZ 	= 1;
+MKL_INT fixedPhiR 	= 1;
+MKL_INT fixedPhiZ 	= 1;
 
 /* INITIAL DATA */
-int readInitialData = 0;
+MKL_INT readInitialData = 0;
 const char *log_alpha_i = NULL;
 const char *beta_i = NULL;
 const char *log_h_i = NULL;
 const char *log_a_i = NULL;
 const char *psi_i = NULL;
 const char *w_i = NULL;
-int NrTotalInitial = 0;
-int NzTotalInitial = 0;
+MKL_INT NrTotalInitial = 0;
+MKL_INT NzTotalInitial = 0;
 
 /* SOLVER PARAMETERS */
-int solverType		= 1;
-int localSolver		= 1;
+MKL_INT solverType		= 1;
+MKL_INT localSolver		= 1;
 double epsilon		= 1E-5;
-int maxNewtonIter 	= 100;
+MKL_INT maxNewtonIter 	= 100;
 double lambda0 		= 1.0E-3;
 double lambdaMin 	= 1.0E-10;
-int useLowRank		= 0;
+MKL_INT useLowRank		= 0;
 
 /* BOUNDARY TYPES */
-int alphaBoundOrder	= 2;
-int betaBoundOrder	= 2;
-int hBoundOrder		= 2;
-int aBoundOrder		= 2;
-int phiBoundOrder	= 2;
+MKL_INT alphaBoundOrder	= 2;
+MKL_INT betaBoundOrder	= 2;
+MKL_INT hBoundOrder		= 2;
+MKL_INT aBoundOrder		= 2;
+MKL_INT phiBoundOrder	= 2;
 
 /* AUXILIARY ARRAYS FOR DERIVATIVES. */
 double *Dr_u;
@@ -69,44 +69,44 @@ extern config_t cfg;
 /* GRID */
 extern double dr;
 extern double dz;
-extern int NrInterior;
-extern int NzInterior;
-extern int NrTotal;
-extern int NzTotal;
-extern int dim;
-extern int ghost;
-extern int order;
+extern MKL_INT NrInterior;
+extern MKL_INT NzInterior;
+extern MKL_INT NrTotal;
+extern MKL_INT NzTotal;
+extern MKL_INT dim;
+extern MKL_INT ghost;
+extern MKL_INT order;
 
 /* SCALAR FIELD PARAMETERS */
-extern int l;
+extern MKL_INT l;
 extern double m;
 extern double psi0;
 extern double sigmaR;
 extern double sigmaZ;
 extern double rExt;
 extern double w0;
-extern int fixedPhiR;
-extern int fixedPhiZ;
+extern MKL_INT fixedPhiR;
+extern MKL_INT fixedPhiZ;
 
 /* INITIAL DATA */
-extern int readInitialData;
+extern MKL_INT readInitialData;
 extern const char *log_alpha_i;
 extern const char *beta_i;
 extern const char *log_h_i;
 extern const char *log_a_i;
 extern const char *psi_i;
 extern const char *w_i;
-extern int NrTotalInitial;
-extern int NzTotalInitial;
+extern MKL_INT NrTotalInitial;
+extern MKL_INT NzTotalInitial;
 
 /* SOLVER PARAMETERS */
-extern int solverType;
-extern int localSolver; 
+extern MKL_INT solverType;
+extern MKL_INT localSolver; 
 extern double epsilon; 
-extern int maxNewtonIter;
+extern MKL_INT maxNewtonIter;
 extern double lambda0;
 extern double lambdaMin;
-extern int useLowRank;
+extern MKL_INT useLowRank;
 
 /* AUXILIARY ARRAYS FOR DERIVATIVES. */
 extern double *Dr_u;
@@ -116,11 +116,11 @@ extern double *Dzz_u;
 extern double *Drz_u;
 
 /* BOUNDARY TYPES */
-extern int alphaBoundOrder;
-extern int betaBoundOrder;
-extern int hBoundOrder;
-extern int aBoundOrder;
-extern int phiBoundOrder;
+extern MKL_INT alphaBoundOrder;
+extern MKL_INT betaBoundOrder;
+extern MKL_INT hBoundOrder;
+extern MKL_INT aBoundOrder;
+extern MKL_INT phiBoundOrder;
 
 /* OUTPUT */
 extern const char *dirname;
