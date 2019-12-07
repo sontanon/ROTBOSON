91bb0009e309daa8c32b0410d1ae16ecad1c9c6f
sontanon <sontanon@gmail.com>
2019-12-07 11:24:49 -0600

    Implement universal symmetry subroutine.

--- src/csr_symmetry.c
+++ src/csr_symmetry.c
@@ -1,60 +1,97 @@
 #include "tools.h"
 
 // Equatorial symmetry.
-void z_symmetry(double *a, MKL_INT *ia, MKL_INT *ja, const MKL_INT offset,
-	const MKL_INT NrTotal, const MKL_INT NzTotal, const MKL_INT dim,
-	const MKL_INT g_num, const MKL_INT i, const MKL_INT j, const MKL_INT z_sym)
+void z_symmetry
+(
+	double *aa,		// CSR array for values.
+	MKL_INT *ia, 		// CSR array for row beginnings.
+	MKL_INT *ja, 		// CSR array for columns.
+	const MKL_INT offset,	// Number of elements filled before.
+	const MKL_INT NrTotal, 	// Grid total dimension in r. 
+	const MKL_INT NzTotal, 	// Grid total dimension in z.
+	const MKL_INT dim,	// Grid total 2D dimension: dim = NrTotal * NzTotal.
+	const MKL_INT g_num, 	// Grid number.
+	const MKL_INT i, 	// Integer coordinate for r: 0 <= i < NrTotal.
+	const MKL_INT j, 	// Integer coordinate for z: 0 <= j < NzTotal.
+	const MKL_INT ghost,	// Number of parity ghost zones.
+	const MKL_INT z_sym	// Equator parity.
+)
 {
 	// Row starts at offset.
 	ia[(g_num - 1) * dim + IDX(i, j)] = BASE + offset;
 
 	// Set values.
-	a[offset    ] = 1.0;
-	a[offset + 1] = -(double)z_sym;
+	aa[offset    ] = 1.0;
+	aa[offset + 1] = -(double)z_sym;
 
 	// Set column values.
 	ja[offset    ] = BASE + (g_num - 1) * dim + IDX(i, j);
-	ja[offset + 1] = BASE + (g_num - 1) * dim + IDX(i, j + 1);
+	ja[offset + 1] = BASE + (g_num - 1) * dim + IDX(i, 2 * ghost - (j + 1));
 
 	// All done.
 	return;
 }
 
 // Radial symmetry.
-void r_symmetry(double *a, MKL_INT *ia, MKL_INT *ja, const MKL_INT offset,
-	const MKL_INT NrTotal, const MKL_INT NzTotal, const MKL_INT dim,
-	const MKL_INT g_num, const MKL_INT i, const MKL_INT j, const MKL_INT r_sym)
+void r_symmetry
+(
+	double *aa,		// CSR array for values.
+	MKL_INT *ia, 		// CSR array for row beginnings.
+	MKL_INT *ja, 		// CSR array for columns.
+	const MKL_INT offset,	// Number of elements filled before.
+	const MKL_INT NrTotal, 	// Grid total dimension in r. 
+	const MKL_INT NzTotal, 	// Grid total dimension in z.
+	const MKL_INT dim,	// Grid total 2D dimension: dim = NrTotal * NzTotal.
+	const MKL_INT g_num, 	// Grid number.
+	const MKL_INT i, 	// Integer coordinate for r: 0 <= i < NrTotal.
+	const MKL_INT j, 	// Integer coordinate for z: 0 <= j < NzTotal.
+	const MKL_INT ghost,	// Number of parity ghost zones.
+	const MKL_INT r_sym	// Axis parity.
+)
 {
 	// Row starts at offset.
 	ia[(g_num - 1) * dim + IDX(i, j)] = BASE + offset;
 
 	// Set values.
-	a[offset    ] = 1.0;
-	a[offset + 1] = -(double)r_sym;
+	aa[offset    ] = 1.0;
+	aa[offset + 1] = -(double)r_sym;
 
 	// Set column values.
 	ja[offset    ] = BASE + (g_num - 1) * dim + IDX(i, j);
-	ja[offset + 1] = BASE + (g_num - 1) * dim + IDX(i + 1, j);
+	ja[offset + 1] = BASE + (g_num - 1) * dim + IDX(2 * ghost - (i + 1), j);
 
 	// All done.
 	return;
 }
 
 // Corner symmetry, i.e. radial plus equatorial symmetry.
-void corner_symmetry(double *a, MKL_INT *ia, MKL_INT *ja, const MKL_INT offset,
-	const MKL_INT NrTotal, const MKL_INT NzTotal, const MKL_INT dim,
-	const MKL_INT g_num, const MKL_INT i, const MKL_INT j, const MKL_INT r_sym, const MKL_INT z_sym)
+void corner_symmetry
+(
+	double *aa,		// CSR array for values.
+	MKL_INT *ia, 		// CSR array for row beginnings.
+	MKL_INT *ja, 		// CSR array for columns.
+	const MKL_INT offset,	// Number of elements filled before.
+	const MKL_INT NrTotal, 	// Grid total dimension in r. 
+	const MKL_INT NzTotal, 	// Grid total dimension in z.
+	const MKL_INT dim,	// Grid total 2D dimension: dim = NrTotal * NzTotal.
+	const MKL_INT g_num, 	// Grid number.
+	const MKL_INT i, 	// Integer coordinate for r: 0 <= i < NrTotal.
+	const MKL_INT j, 	// Integer coordinate for z: 0 <= j < NzTotal.
+	const MKL_INT ghost,	// Number of parity ghost zones.
+	const MKL_INT r_sym,	// Axis parity.
+	const MKL_INT z_sym	// Equator parity.
+)
 {
 	// Row starts at offset.
 	ia[(g_num - 1) * dim + IDX(i, j)] = BASE + offset;
 
 	// Set values.
-	a[offset    ] = 1.0;
-	a[offset + 1] = -(double)(r_sym * z_sym);
+	aa[offset    ] = 1.0;
+	aa[offset + 1] = -(double)(r_sym * z_sym);
 
 	// Set column values.
 	ja[offset    ] = BASE + (g_num - 1) * dim + IDX(i, j);
-	ja[offset + 1] = BASE + (g_num - 1) * dim + IDX(i + 1, j + 1);
+	ja[offset + 1] = BASE + (g_num - 1) * dim + IDX(2 * ghost - (i + 1), 2 * ghost - (j + 1));
 
 	// All done.
 	return;
--- src/csr_symmetry.h
+++ src/csr_symmetry.h
@@ -1,11 +1,42 @@
-void z_symmetry(double *a, MKL_INT *ia, MKL_INT *ja, const MKL_INT offset,
-	const MKL_INT NrTotal, const MKL_INT NzTotal, const MKL_INT dim,
-	const MKL_INT g_num, const MKL_INT i, const MKL_INT j, const MKL_INT z_sym);
+void z_symmetry(
+	double *aa,		// CSR array for values.
+	MKL_INT *ia, 		// CSR array for row beginnings.
+	MKL_INT *ja, 		// CSR array for columns.
+	const MKL_INT offset,	// Number of elements filled before.
+	const MKL_INT NrTotal, 	// Grid total dimension in r. 
+	const MKL_INT NzTotal, 	// Grid total dimension in z.
+	const MKL_INT dim,	// Grid total 2D dimension: dim = NrTotal * NzTotal.
+	const MKL_INT g_num, 	// Grid number.
+	const MKL_INT i, 	// Integer coordinate for r: 0 <= i < NrTotal.
+	const MKL_INT j, 	// Integer coordinate for z: 0 <= j < NzTotal.
+	const MKL_INT ghost,	// Number of parity ghost zones.
+	const MKL_INT z_sym);	// Equator parity.
 
-void r_symmetry(double *a, MKL_INT *ia, MKL_INT *ja, const MKL_INT offset,
-	const MKL_INT NrTotal, const MKL_INT NzTotal, const MKL_INT dim,
-	const MKL_INT g_num, const MKL_INT i, const MKL_INT j, const MKL_INT r_sym);
+void r_symmetry(
+	double *aa,		// CSR array for values.
+	MKL_INT *ia, 		// CSR array for row beginnings.
+	MKL_INT *ja, 		// CSR array for columns.
+	const MKL_INT offset,	// Number of elements filled before.
+	const MKL_INT NrTotal, 	// Grid total dimension in r. 
+	const MKL_INT NzTotal, 	// Grid total dimension in z.
+	const MKL_INT dim,	// Grid total 2D dimension: dim = NrTotal * NzTotal.
+	const MKL_INT g_num, 	// Grid number.
+	const MKL_INT i, 	// Integer coordinate for r: 0 <= i < NrTotal.
+	const MKL_INT j, 	// Integer coordinate for z: 0 <= j < NzTotal.
+	const MKL_INT ghost,	// Number of parity ghost zones.
+	const MKL_INT r_sym);	// Axis parity.
 
-void corner_symmetry(double *a, MKL_INT *ia, MKL_INT *ja, const MKL_INT offset,
-	const MKL_INT NrTotal, const MKL_INT NzTotal, const MKL_INT dim,
-	const MKL_INT g_num, const MKL_INT i, const MKL_INT j, const MKL_INT r_sym, const MKL_INT z_sym);
+void corner_symmetry(
+	double *aa,		// CSR array for values.
+	MKL_INT *ia, 		// CSR array for row beginnings.
+	MKL_INT *ja, 		// CSR array for columns.
+	const MKL_INT offset,	// Number of elements filled before.
+	const MKL_INT NrTotal, 	// Grid total dimension in r. 
+	const MKL_INT NzTotal, 	// Grid total dimension in z.
+	const MKL_INT dim,	// Grid total 2D dimension: dim = NrTotal * NzTotal.
+	const MKL_INT g_num, 	// Grid number.
+	const MKL_INT i, 	// Integer coordinate for r: 0 <= i < NrTotal.
+	const MKL_INT j, 	// Integer coordinate for z: 0 <= j < NzTotal.
+	const MKL_INT ghost,	// Number of parity ghost zones.
+	const MKL_INT r_sym, 	// Axis parity.
+	const MKL_INT z_sym);	// Equator parity.
