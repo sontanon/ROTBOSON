73e1cfdfd6310ca301b61e20ef74224b660786e5
sontanon <sontanon@gmail.com>
2020-09-04 10:11:23 -0500

    Fix du coupling overflow but turn off coupling.

--- src/regularization_coupling.h
+++ src/regularization_coupling.h
@@ -1,5 +1,7 @@
 // Regularization coupling.
-#define REGULARIZATION_COUPLING
+#undef REGULARIZATION_COUPLING
+
+#ifdef REGULARIZATION_COUPLING
 #define REG_MU 0.5
 
 // GRID
@@ -13,4 +15,5 @@ extern double solver_dr;
 extern MKL_INT solver_NrTotal;
 extern MKL_INT solver_NzTotal;
 extern MKL_INT solver_ghost;
+#endif
 #endif
--- src/tools.c
+++ src/tools.c
@@ -53,11 +53,12 @@ void array_sum(double *z, const double alpha, double *x, const double beta, doub
 void coupled_du(double *du, double *u, const MKL_INT NrTotal, const MKL_INT NzTotal, const MKL_INT ghost, const double dr, const double mu)
 {
 	MKL_INT dim = NrTotal * NzTotal;
-	MKL_INT i, j, k;
+	MKL_INT i = 0;
+	MKL_INT j = 0;
 	double r;
-	#pragma omp parallel shared(du) private(r)
+	#pragma omp parallel shared(du) private(r, j)
 	{
-		#pragma omp for scheudle(dynamic, 1)
+		#pragma omp for schedule(dynamic, 1)
 		for (i = 0; i < NrTotal; ++i)
 		{
 			r = dr * (i - ghost + 0.5);
@@ -65,7 +66,7 @@ void coupled_du(double *du, double *u, const MKL_INT NrTotal, const MKL_INT NzTo
 			{
 				du[3 * dim + IDX(i, j)] *= (1.0 - mu);
 				// 2 * a**2 * d(log(a)) = r**2 * d(lambda) + 2 * h**2 * d(log(h)).
-				du[3 * dim + IDX(i, j)] += mu * (0.5 * r * r * du[6 * dim + IDX(i, j)] + exp(2.0 * u[2 * dim + IDX(i, j)]) * du[2 * dim + IDX(i, j)]) / exp(2.0 * u[3 * dim + IDX(i, j)]);
+				du[3 * dim + IDX(i, j)] += mu * (0.5 * r * r * du[5 * dim + IDX(i, j)] + exp(2.0 * u[2 * dim + IDX(i, j)]) * du[2 * dim + IDX(i, j)]) / exp(2.0 * u[3 * dim + IDX(i, j)]);
 			}
 		}
 	}
